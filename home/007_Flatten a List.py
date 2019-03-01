2dac00b5cb46e3790ce066a22b22eb4dcec682be
jiayiwangjw <JIAYI0206@ME.COM>
2019-03-01 13:15:25 -0500

    Create 007_Flatten a List.py

---
+++ home/007_Flatten a List.py
@@ -0,0 +1,27 @@
+"""
+Input data: A nested list with integers.
+
+Output data: The one-dimensional list with integers.
+
+Example:
+
+flat_list([1, 2, 3]) == [1, 2, 3]
+flat_list([1, [2, 2, 2], 4]) == [1, 2, 2, 2, 4]
+flat_list([[[2]], [4, [5, 6, [6], 6, 6, 6], 7]]) == [2, 4, 5, 6, 6, 6, 6, 6, 7]
+flat_list([-1, [1, [-2], 1], -1]) == [-1, 1, -2, 1, -1]
+"""
+
+
+def flat_list(x):
+    
+     result=[]
+     for i in x:
+         result += flat_list(i) if isinstance(i,list) else [i]
+     return result
+
+if __name__ == '__main__':
+    assert flat_list([1, 2, 3]) == [1, 2, 3], "First"
+    assert flat_list([1, [2, 2, 2], 4]) == [1, 2, 2, 2, 4], "Second"
+    assert flat_list([[[2]], [4, [5, 6, [6], 6, 6, 6], 7]]) == [2, 4, 5, 6, 6, 6, 6, 6, 7], "Third"
+    assert flat_list([-1, [1, [-2], 1], -1]) == [-1, 1, -2, 1, -1], "Four"
+    print('Done! Check it')
